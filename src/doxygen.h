54999cb80447e70e72e30f1b6ab55a29844220e7
Armin Hornung <hornunga@informatik.uni-freiburg.de>
2009-09-29 16:32:11 +0000

    More doxygen

---
+++ src/doxygen.h
@@ -0,0 +1,36 @@
+/**
+ * This file is intentionally left blank ;)
+ * (just containing some doxygen definitions)
+ *
+ *
+ * @defgroup math Math
+ *
+ * @defgroup octomap Octomap
+ *
+ * @defgroup visualization Visualization
+ *
+ */
+
+/** \mainpage Octomap
+
+  \section intro_sec Introduction
+
+  The <a href="http://octomap.sourceforge.net/">Octomap library</a> implements a 3D occupancy grid mapping approach. It provides data structures and mapping algorithms. The map is
+  implemented using an \ref octomap::OcTree "Octree".
+
+  \section install_sec Installation
+    See the file README.txt in the main folder.
+
+**/
+
+/**
+ * \namespace octomath Namespace of the mini-math library in Octomap
+ *
+ */
+
+/**
+ * \namespace octomap Namespace the Octomap library
+ *
+ */
+
+
