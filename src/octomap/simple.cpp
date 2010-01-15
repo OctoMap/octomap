ece5d2adc3a4669a458abab6aa5e5ba2f23cf7c0
Kai Wurm <wurm@informatik.uni-freiburg.de>
2010-01-15 11:19:05 +0000

    added queries to example

--- src/octomap/simple.cpp
+++ src/octomap/simple.cpp
@@ -33,6 +33,9 @@ using namespace octomap;
 int main(int argc, char** argv) {
 
 
+  cout << endl;
+  cout << "generating example map" << endl;
+
   OcTree tree (0.1);  // create empty tree with resolution 0.1
 
 
@@ -58,10 +61,34 @@ int main(int argc, char** argv) {
     }
   }
 
+  cout << endl;
+  cout << "performing some queries:" << endl;
+
   point3d query (0., 0., 0.);
-    
+  OcTreeNode* result = tree.search (query);
+  if (result != NULL) 
+    cout << "occupancy probability at " << query << ":    " << result->getOccupancy() << endl;
+  else 
+    cout << "occupancy probability at " << query << ":    is unknown" << endl;    
+
+  query = point3d(-1.,-1.,-1.);
+  result = tree.search (query);
+  if (result != NULL) 
+    cout << "occupancy probability at " << query << ": " << result->getOccupancy() << endl;
+  else 
+    cout << "occupancy probability at " << query << ":    is unknown" << endl;    
+
+  query = point3d(1.,1.,1.);
+  result = tree.search (query);
+  if (result != NULL) 
+    cout << "occupancy probability at " << query << ":    " << result->getOccupancy() << endl;
+  else 
+    cout << "occupancy probability at " << query << ":    is unknown" << endl;    
 
 
+
+
+  cout << endl;
   tree.writeBinary("simple_tree.bt");
   cout << "wrote example file simple_tree.bt" << endl << endl;
   cout << "now you can use octovis to visualize: octovis simple_tree.bt" << endl << endl;
