8e6781a35869a4161b59f7af3d233722626e48fa
Armin Hornung <hornunga@informatik.uni-freiburg.de>
2011-07-22 11:55:44 +0000

    small cleanup of CMakeLists into submodules

---
+++ CMakeModules/CompilerSettings.cmake
@@ -0,0 +1,28 @@
+# COMPILER SETTINGS (default: Release)
+# use "-DCMAKE_BUILD_TYPE=Debug" in cmake for a Debug-build
+IF(NOT CMAKE_CONFIGURATION_TYPES AND NOT CMAKE_BUILD_TYPE)
+   SET(CMAKE_BUILD_TYPE Release)
+ENDIF(NOT CMAKE_CONFIGURATION_TYPES AND NOT CMAKE_BUILD_TYPE)
+
+# COMPILER FLAGS
+SET (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wno-error ")
+SET (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wno-error ")
+SET (CMAKE_CXX_FLAGS_RELEASE "-O3 -funroll-loops -DNDEBUG -O3 -msse3 -mssse3")
+SET (CMAKE_CXX_FLAGS_DEBUG "-O0 -g")
+
+MESSAGE ("\n")
+MESSAGE (STATUS "Building as ${CMAKE_BUILD_TYPE}")
+MESSAGE ("\n")
+
+# Shared object compilation under 64bit (vtable)
+ADD_DEFINITIONS(-fPIC)	
+
+# Set full rpath http://www.paraview.org/Wiki/CMake_RPATH_handling
+# (good to have and required with ROS)
+set(CMAKE_SKIP_BUILD_RPATH  FALSE)
+set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
+set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+
+# no prefix needed for python modules
+set(CMAKE_SHARED_MODULE_PREFIX "")
