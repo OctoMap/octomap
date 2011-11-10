29f9e538c4447808eafe1113350b8eaa4e421841
Christoph Sprunk <sprunkc@informatik.uni-freiburg.de>
2011-11-10 15:38:20 +0000

    adding missing files

---
+++ octomap/src/testing/test_mapcollection.cpp
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <octomap/MapCollection.h>
+
+using namespace std;
+using namespace octomap;
+using namespace octomath;
+
+int main(int argc, char** argv) {
+
+	std::string filename(argv[1]);
+
+	MapCollection<MapNode<OcTree> > collection(filename);
+
+	collection.writePointcloud("test.vrml");
+
+
+
+
+  return 0;
+}
