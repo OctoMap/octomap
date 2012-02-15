44a64231db9ea5fc22692f32a032c3e473e117fc
Joerg Mueller <muellerj@informatik.uni-freiburg.de>
2012-02-15 14:34:10 +0000

    fixed bug in function reading legacy binary files

--- octomap/include/octomap/OccupancyOcTreeBase.hxx
+++ octomap/include/octomap/OccupancyOcTreeBase.hxx
@@ -752,6 +752,7 @@ namespace octomap {
       OCTOMAP_DEBUG_STR("Reading binary octree type "<< id);
     } else{ // try to read old binary format:
       s.seekg(streampos);
+      s.clear(); // clear eofbit of istream
       if (readBinaryLegacyHeader(s, size, res)){
         OCTOMAP_WARNING_STR("You are using an outdated binary tree file format.");
         OCTOMAP_WARNING_STR("Please convert your .bt files with convert_octree.");
