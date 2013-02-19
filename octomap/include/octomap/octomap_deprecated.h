6bbf7600e9430cda0741785f9c2e659a5f2b3790
Armin Hornung <hornunga@informatik.uni-freiburg.de>
2013-02-19 15:01:57 +0100

    Macro DEPRECATED replaced with OCTOMAP_DEPRECATED for better compatibility

--- octomap/include/octomap/octomap_deprecated.h
+++ octomap/include/octomap/octomap_deprecated.h
@@ -35,14 +35,14 @@
 #define OCTOMAP_DEPRECATED_H
 
 // define multi-platform deprecation mechanism
-#ifndef DEPRECATED
+#ifndef OCTOMAP_DEPRECATED
   #ifdef __GNUC__
-    #define DEPRECATED(func) func __attribute__ ((deprecated))
+    #define OCTOMAP_DEPRECATED(func) func __attribute__ ((deprecated))
   #elif defined(_MSC_VER)
-    #define DEPRECATED(func) __declspec(deprecated) func
+    #define OCTOMAP_DEPRECATED(func) __declspec(deprecated) func
   #else
-    #pragma message("WARNING: You need to implement DEPRECATED for this compiler")
-    #define DEPRECATED(func) func
+    #pragma message("WARNING: You need to implement OCTOMAP_DEPRECATED for this compiler")
+    #define OCTOMAP_DEPRECATED(func) func
   #endif
 #endif
 
