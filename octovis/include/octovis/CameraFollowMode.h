54430943fd0c0e88fddb495572315fc392091e59
Armin Hornung <hornunga@informatik.uni-freiburg.de>
2013-04-09 16:18:32 +0200

    Indenting CameraFollowMode

--- octovis/include/octovis/CameraFollowMode.h
+++ octovis/include/octovis/CameraFollowMode.h
@@ -29,51 +29,51 @@
 #include "SceneObject.h"
 
 class CameraFollowMode : public QObject {
-	Q_OBJECT
+  Q_OBJECT
 
 public:
-	CameraFollowMode(octomap::ScanGraph *graph = NULL);
-	virtual ~CameraFollowMode();
-	void setScanGraph(octomap::ScanGraph *graph);
+  CameraFollowMode(octomap::ScanGraph *graph = NULL);
+  virtual ~CameraFollowMode();
+  void setScanGraph(octomap::ScanGraph *graph);
 
 public slots:
-	void jumpToFrame(unsigned int frame);
-	void cameraPathStopped(int id);
-	void cameraPathFrameChanged(int id, int current_camera_frame);
-	void play();
-	void pause();
-	void clearCameraPath();
-	void saveToCameraPath();
-	void addToCameraPath();
-	void removeFromCameraPath();
-	void followCameraPath();
-	void followRobotPath();
+  void jumpToFrame(unsigned int frame);
+  void cameraPathStopped(int id);
+  void cameraPathFrameChanged(int id, int current_camera_frame);
+  void play();
+  void pause();
+  void clearCameraPath();
+  void saveToCameraPath();
+  void addToCameraPath();
+  void removeFromCameraPath();
+  void followCameraPath();
+  void followRobotPath();
 
 signals:
-	void changeCamPose(const octomath::Pose6D& pose);
-	void interpolateCamPose(const octomath::Pose6D& old_pose, const octomath::Pose6D& new_pose, double u);
-	void stopped();
-	void frameChanged(unsigned int frame);
-	void deleteCameraPath(int id);
-	void removeFromCameraPath(int id, int frame);
-	void updateCameraPath(int id, int frame);
-    void appendToCameraPath(int id, const octomath::Pose6D& pose);
-    void appendCurrentToCameraPath(int id);
-    void addCurrentToCameraPath(int id, int frame);
-    void playCameraPath(int id, int start_frame);
-    void stopCameraPath(int id);
-    void jumpToCamFrame(int id, int frame);
-    void changeNumberOfFrames(unsigned count);
-    void scanGraphAvailable(bool available);
+  void changeCamPose(const octomath::Pose6D& pose);
+  void interpolateCamPose(const octomath::Pose6D& old_pose, const octomath::Pose6D& new_pose, double u);
+  void stopped();
+  void frameChanged(unsigned int frame);
+  void deleteCameraPath(int id);
+  void removeFromCameraPath(int id, int frame);
+  void updateCameraPath(int id, int frame);
+  void appendToCameraPath(int id, const octomath::Pose6D& pose);
+  void appendCurrentToCameraPath(int id);
+  void addCurrentToCameraPath(int id, int frame);
+  void playCameraPath(int id, int start_frame);
+  void stopCameraPath(int id);
+  void jumpToCamFrame(int id, int frame);
+  void changeNumberOfFrames(unsigned count);
+  void scanGraphAvailable(bool available);
 
 
 protected:
-	octomap::ScanGraph *m_scan_graph;
-	unsigned int m_current_scan;
-	unsigned int m_current_cam_frame;
-	unsigned int m_number_cam_frames;
-	unsigned int m_start_frame;
-	bool m_followRobotTrajectory;
+  octomap::ScanGraph *m_scan_graph;
+  unsigned int m_current_scan;
+  unsigned int m_current_cam_frame;
+  unsigned int m_number_cam_frames;
+  unsigned int m_start_frame;
+  bool m_followRobotTrajectory;
 };
 
 #endif /* CAMERAFOLLOWMODE_H_ */
