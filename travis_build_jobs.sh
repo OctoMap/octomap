36f1d01bf3f8e4b995f06811885983aab0a497d2
Armin Hornung <armin@hornung.io>
2016-05-04 23:13:33 +0200

    Travis build variants

--- travis_build_jobs.sh
+++ travis_build_jobs.sh
@@ -4,20 +4,36 @@
 
 set -e
 
-if [ -z "$1" ]
-then
-  DIR="."
-else
-  DIR=$1
-fi
+function build {
+  cd $1
+  mkdir build
+  cd build
+  cmake .. -DCMAKE_INSTALL_PREFIX=/tmp/octomap/$1
+  make -j4
+  cd ..
+}
 
-cd $DIR
-mkdir build
-cd build
-cmake ..
-make -j4
+case "$1" in
+"dist")
+  build .
+  cd build && make test
+  make install
+  ;;
+"components")
+  build octomap
+  cd build && make test
+  make install
+  cd ../..
+  build dynamicEDT3D
+  cd ..
+  build octovis
+  cd ..
+  ;;
+*)
+  echo "Invalid build variant"
+  exit 1
+esac
 
-# TODO: add test and install jobs
 
 
 
