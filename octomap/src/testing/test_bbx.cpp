db60c6ade75b2fcbd813de01093cece0a39971b2
Kyle Julian <kjulian3@stanford.edu>
2022-01-27 13:29:38 -0800

    Use coarser presicion for equality tests.

--- octomap/src/testing/test_bbx.cpp
+++ octomap/src/testing/test_bbx.cpp
@@ -39,23 +39,24 @@ int main(int /*argc*/, char** /*argv*/) {
   // Searching in the x-direction from the origin finds the first point.
   bool ray_cast_ret = tree.castRay(origin, point3d(1.f, 0.f, 0.f), end_point);
   EXPECT_TRUE(ray_cast_ret);
-  EXPECT_FLOAT_EQ(end_point.x(), point0.x());
-  EXPECT_FLOAT_EQ(end_point.y(), point0.y());
-  EXPECT_FLOAT_EQ(end_point.z(), point0.z());
+  const float eps = 1e-3;
+  EXPECT_NEAR(end_point.x(), point0.x(), eps);
+  EXPECT_NEAR(end_point.y(), point0.y(), eps);
+  EXPECT_NEAR(end_point.z(), point0.z(), eps);
 
   // Searching in the y-direction from the origin terminates just outside the
   // bounding box.
   ray_cast_ret = tree.castRay(origin, point3d(0.f, 1.f, 0.f), end_point);
   EXPECT_FALSE(ray_cast_ret);
-  EXPECT_FLOAT_EQ(end_point.x(), point1.x());
-  EXPECT_FLOAT_EQ(end_point.y(), bbx_limit + resolution);
-  EXPECT_FLOAT_EQ(end_point.z(), point1.z());
+  EXPECT_NEAR(end_point.x(), point1.x(), eps);
+  EXPECT_NEAR(end_point.y(), bbx_limit + resolution, eps);
+  EXPECT_NEAR(end_point.z(), point1.z(), eps);
 
   // Searching in the z-direction from the origin terminates at the max range.
   ray_cast_ret = tree.castRay(origin, point3d(0.f, 0.f, 1.f), end_point);
   EXPECT_FALSE(ray_cast_ret);
-  EXPECT_FLOAT_EQ(end_point.x(), point2.x());
-  EXPECT_FLOAT_EQ(end_point.y(), point2.y());
-  EXPECT_FLOAT_EQ(end_point.z(), origin.z() + maxrange);
+  EXPECT_NEAR(end_point.x(), point2.x(), eps);
+  EXPECT_NEAR(end_point.y(), point2.y(), eps);
+  EXPECT_NEAR(end_point.z(), origin.z() + maxrange, eps);
   return 0;
 }
